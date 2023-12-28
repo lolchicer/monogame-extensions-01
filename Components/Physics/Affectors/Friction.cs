f92ac6944b56172a614e714ab5ae1ecf66c5b0f3
lolchicer <lolchicer@live.ru>
2023-12-28 12:22:13 +0300

    выведение остановки объекта в лишний кадр в Friction.UpdateVelocity(...); Linking

--- Components/Physics/Affectors/Friction.cs
+++ Components/Physics/Affectors/Friction.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 
 namespace MonogameTest01;
@@ -5,18 +6,26 @@ namespace MonogameTest01;
 public class Friction : Affector
 {
     private const float _speed = 2;
+    private List<Linking> _linkings = new();
 
     protected override void UpdateVelocity(GameTime gameTime)
     {
+        foreach (var linking in _linkings)
+        {
+            linking.Update(gameTime);
+            _linkings.Remove(linking);
+        }
+
         if (_mechanics.Velocity.Length() == 0)
             return;
-        else if (_mechanics.Velocity.Length() < _speed)
-            _velocity = -_mechanics.Velocity;
         else
         {
             var normalizedVelocity = _mechanics.Velocity;
             normalizedVelocity.Normalize();
             _velocity = -normalizedVelocity * _speed;
+
+            if (_mechanics.Velocity.Length() - _speed <= 0)
+                _linkings.Add(new Linking(-(_mechanics.Velocity - _velocity), _mechanics));
         }
     }
 
---
+++ Components/Physics/Affectors/Linking.cs
@@ -0,0 +1,19 @@
+using Microsoft.Xna.Framework;
+
+namespace MonogameTest01;
+
+public class Linking : Affector
+{
+    private Vector2 _linkVelocity;
+    
+    protected override void UpdateVelocity(GameTime gameTime)
+    {
+        _velocity = _linkVelocity;
+    }
+
+    public Linking(Vector2 velocity, Mechanics mechanics)
+    : base(mechanics)
+    {
+        _linkVelocity = velocity;
+    }
+}
