96e839a41c329f9e69947e5f5fb7e6136b0c6e76
lolchicer <lolchicer@live.ru>
2024-03-05 15:38:46 +0300

    удаление History как класса GameComponent; реорганизация Level

--- Components/History.cs
+++ Components/History.cs
@@ -4,24 +4,19 @@ using Microsoft.Xna.Framework;
 
 namespace MonogameTest01;
 
-public class History : GameComponent
+public class History
 {
     private IList<Action> _actions = new List<Action>();
-    private void Do()
-    {
-        foreach (var action in _actions)
-            action();
-    }
-
+    
     public void Add(Action value) =>
     _actions.Add(value);
 
-    public override void Update(GameTime gameTime)
+    public void Do()
     {
-        Do();
+        foreach (var action in _actions)
+            action();
         _actions.Clear();
     }
 
-    public History(Game game)
-    : base(game) { }
+    public History() { }
 }
--- Components/Level.cs
+++ Components/Level.cs
@@ -1,47 +1,50 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Microsoft.Xna.Framework;
 
 namespace MonogameTest01;
 
 public class Level : DrawableGameComponent
 {
-    private History _history;
+    private History _history = new();
     private Player _player;
 
     public History History => _history;
+    
     public Player Player => _player;
     public List<Projectile> Projectiles { get; } = new();
-    public List<Action> Actions { get; } = new();
     public List<Entity> Entities { get; } = new();
+    
+    protected IEnumerable<IUpdateable> Updateables()
+    {
+        var value = new IUpdateable[Projectiles.Count + Entities.Count];
+        Array.Copy(Projectiles.ToArray(), value, Projectiles.Count);
+        Array.Copy(Entities.ToArray(), 0, value, Projectiles.Count, Entities.Count);
+        return value;
+    }
+
+    protected IEnumerable<IDrawable> Drawables() =>
+    Entities.ToArray();
 
     public override void Update(GameTime gameTime)
     {
-        _player.Update(gameTime);
-        foreach (var projectile in Projectiles.ToArray())
-            projectile.Update(gameTime);
-        Entities.ForEach(entity => entity.Update(gameTime));
-        _history.Update(gameTime);
+        foreach (var updateable in Updateables())
+            updateable.Update(gameTime);
+        _history.Do();
 
         base.Update(gameTime);
     }
 
-    public override void Initialize()
-    {
-        base.Initialize();
-    }
-
     public override void Draw(GameTime gameTime)
     {
-        Entities.ForEach(entity => entity.Draw(gameTime));
+        foreach (var drawable in Drawables())
+            drawable.Draw(gameTime);
 
         base.Draw(gameTime);
     }
 
     public Level(Game game) : base(game)
     {
-        _history = new History(game);
         _player = new Player(_history);
     }
 }
